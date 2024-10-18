faed42d87761a9abccedd9220d2fbe467c1efd6d
Gagan Nandwana <116247553+gagan244@users.noreply.github.com>
2024-10-18 09:52:16 +0530

    Add files via upload

---
+++ select_statement.sql
@@ -0,0 +1,13 @@
+select *
+FROM parks_and_recreation.employee_demographics;
+
+select first_name,
+last_name,
+birth_date,
+age,
+(age + 10) * 10 +10
+FROM parks_and_recreation.employee_demographics;
+#PEMDAS
+
+select distinct first_name, gender
+FROM parks_and_recreation.employee_demographics;
